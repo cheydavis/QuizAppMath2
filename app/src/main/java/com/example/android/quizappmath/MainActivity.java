80a1586dcd6e1693ea9b856a40663caa2c0f68c6
Chey <chey.davis1998@gmail.com>
2018-08-17 18:37:04 -0400

    last submit

--- app/src/main/java/com/example/android/quizappmath/MainActivity.java
+++ app/src/main/java/com/example/android/quizappmath/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.quizappmath;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -21,11 +22,6 @@ public class MainActivity extends AppCompatActivity {
             rb1,
             rb3,
             answer2;
-    //question 3
-    CheckBox cb1,
-            cb2,
-            cb3,
-            cb4;
     //question 4
     EditText editText;
     //submit button
@@ -41,45 +37,34 @@ public class MainActivity extends AppCompatActivity {
 
     public void submitAnswers(View view) {
 
-        TextView textView = findViewById(R.id.scoreTotal);
-        int firstAnswer_score;
-        int secondAnswer_score;
-        int thirdAnswer_score;
-        int fourthAnswer_score;
-        int scoreTotal;
+        Button display = findViewById(R.id.scoreTotal);
+        int scoreTotal = 0, firstAnswer_score, secondAnswer_score, thirdAnswer_score, fourthAnswer_score;
+
+        CharSequence display;
 
         // Question 1
-        boolean firstAnswer;
+        Boolean firstAnswer;
         firstAnswer = rb1.isChecked();
         if (firstAnswer) {
             firstAnswer_score = 1;
-        } else {
-            firstAnswer_score = 0;
         }
         // Question 2
         boolean secondAnswer;
         secondAnswer = rb3.isChecked();
         if (secondAnswer) {
             secondAnswer_score = 1;
-        } else {
-            secondAnswer_score = 0;
         }
 
         //Question 3
+        CheckBox thirdAnswer_check2 = findViewById(R.id.cb2);
+        CheckBox thirdAnswer_check4 = findViewById(R.id.cb4);
 
-        Boolean thirdAnswer_check1, thirdAnswer_check2, thirdAnswer_check3, thirdAnswer_check4;
-        thirdAnswer_check1 = cb1.isChecked();
-        thirdAnswer_check2 = cb2.isChecked();
-        thirdAnswer_check3 = cb3.isChecked();
-        thirdAnswer_check4 = cb4.isChecked();
+        thirdAnswer_check2.isChecked();
+        thirdAnswer_check4.isChecked();
 
-        if (!thirdAnswer_check1.isChecked()
-                && thirdAnswer_check2.isChecked()
-                && !thirdAnswer_check3.isChecked()
+        if (thirdAnswer_check2.isChecked()
                 && thirdAnswer_check4.isChecked()) {
             thirdAnswer_score = 1;
-        } else {
-            thirdAnswer_score = 0;
         }
 
     }// Question 4
@@ -88,17 +73,22 @@ public class MainActivity extends AppCompatActivity {
         EditText editText = (EditText) findViewById(R.id.editText);
 
         String fourthAnswer = editText.getText().toString();
-        String answer = "";
-        boolean isAnswerTrue = answer.equals(fourthAnswer);
+        String answer = "22";
     }
-}
+    }
 // Toast Message Displaying Results
 
-    scoreTotal=firstAnswer_score+secondAnswer_score+thirdAnswer_score+fourthAnswer_score;
-            if(scoreTotal = 4){
+    scoreTotal =firstAnswer_score +secondAnswer_score +thirdAnswer_score+fourthAnswer_score;{
+            if(scoreTotal=4)
+            {
             display="Good job, you got them all right!";
-            }else{
-            display="Oops try again"}
-
-            TextView.setText("Your score: " + scoreTotal);
+            }else
+            {
+            display="Oops try again";
             }
+            Toast toast=Toast.makeText(getApplicationContext(),display,Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER,0,0);
+            toast.show();
+            }
+
+
